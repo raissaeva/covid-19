6a5ec7f4a165c17345bc72ee2919c59ffd4aee03
raissaeva <63860092+raissaeva@users.noreply.github.com>
2020-04-25 13:37:44 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Checkup Yourself</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+<div class = "row"> 
+<div class = "col-md-6">
+<center>
+        <h2>Hallo Semua!!</h2>
+     
+            <img src="foto1.jpg" width="200" height="200" >
+        <h5><p>Saya Raissa Eva Ramadhanty Sukmana</p>
+            <p>Lahir di Malang,15 November 2003</p>
+            <p>Saya siswi SMK Telkom Malang kelas XRPL7</p>
+            <p>Untuk kalian semua stay at home aja ya,makan makanan yang sehat,jaga kebersihan,hindari interaksi langsung,dan jangan lupa untuk cuci tangan</p>
+            
+            <p>"Pisau menggores menjadi luka,rasanya sakit amatlah pedih             Cukup sekian dari saya,saya ucapkan terimakasih"</p>
+          </h5>
+          </center>
+          <center>
+          <p> <a href="./index.php"class="btn btn-warning">Back</a></p>
+          </center>
+        </div>
+</div>
+       
+       
+  <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
